85f91a640000fe0961113addec0d44338270fea5
zhouwenlai <zhouwenlai@chehejia.com>
2018-08-17 14:30:38 +0800

    add deps

--- boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
+++ boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
@@ -2,7 +2,10 @@ package com.framework.dynamic.boot;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.bootstrap.ServerBootstrapConfig;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.oio.OioEventLoopGroup;
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
 
@@ -20,7 +23,7 @@ public class Bootstrap {
         ServerBootstrap bs = new ServerBootstrap();
         EpollEventLoopGroup parent = new EpollEventLoopGroup(2);
 
-        bs.group(null, null);
+        bs.group(parent, null);
         System.out.println(bs.config().toString());
     }
 }
