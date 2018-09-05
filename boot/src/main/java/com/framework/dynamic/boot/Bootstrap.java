ea1dc3bc216c6c3e0d7a9db69278713beff49d06
zhouwenlai <zhouwenlai@chehejia.com>
2018-09-05 14:27:26 +0800

    add deps

--- boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
+++ boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
@@ -1,24 +1,13 @@
 package com.framework.dynamic.boot;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.bootstrap.ServerBootstrapConfig;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.EpollEventLoopGroup;
-import io.netty.channel.nio.NioEventLoop;
-import io.netty.channel.oio.OioEventLoopGroup;
-import io.undertow.Undertow;
-import io.undertow.util.Headers;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootConfiguration
 public class Bootstrap {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws Exception {
         //Undertow server = Undertow.builder()
         //        .addHttpListener(8080, "localhost")
         //        .setHandler(exchange -> {
@@ -26,9 +15,10 @@ public class Bootstrap {
         //            exchange.getResponseSender().send("Hello World");
         //        }).build();
         //server.start();
-
-        SpringApplication app = new SpringApplication(Bootstrap.class);
-        app.run();
+        //GroovyScriptEngine engine = new GroovyScriptEngine();
+        //engine.getGroovyClassLoader().parseClass(
+        //engine.run("", "");
+        new SpringApplicationBuilder(Bootstrap.class).web(WebApplicationType.SERVLET).build().run(args);
 
     }
 }
