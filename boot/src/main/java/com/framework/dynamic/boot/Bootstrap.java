b00fad4b65f94828a37939f9c4b11fb2655244eb
zhouwenlai <zhouwenlai@chehejia.com>
2018-09-17 11:11:28 +0800

    add deps

--- boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
+++ boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
@@ -1,5 +1,8 @@
 package com.framework.dynamic.boot;
 
+import ch.qos.logback.classic.servlet.LogbackServletContextListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -18,7 +21,12 @@ public class Bootstrap {
         //GroovyScriptEngine engine = new GroovyScriptEngine();
         //engine.getGroovyClassLoader().parseClass(
         //engine.run("", "");
-        new SpringApplicationBuilder(Bootstrap.class).web(WebApplicationType.SERVLET).build().run(args);
+        //new SpringApplicationBuilder(Bootstrap.class).web(WebApplicationType.SERVLET).build().run(args);
 
+        System.setProperty("log.base", "d:/log");
+        Logger log = LoggerFactory.getLogger("ROOT");
+        for (int i = 0; i < 20; i++) {
+            log.error("hello world");
+        }
     }
 }
