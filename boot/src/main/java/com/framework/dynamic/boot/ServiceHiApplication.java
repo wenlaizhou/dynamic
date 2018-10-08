ebca693cec0566a2ecb0124baca54621df3686da
zhouwenlai <zhouwenlai@chehejia.com>
2018-10-08 14:18:42 +0800

    新增swagger支持

--- boot/src/main/java/com/framework/dynamic/boot/ServiceHiApplication.java
+++ boot/src/main/java/com/framework/dynamic/boot/ServiceHiApplication.java
@@ -1,17 +1,31 @@
 package com.framework.dynamic.boot;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableSwagger2
+@Api(description = "测试接口", value = "测试测试")
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
@@ -21,7 +35,27 @@ public class ServiceHiApplication {
     @Value("${server.port}")
     String port;
 
-    @RequestMapping("/hi")
+
+    @Bean
+    public Docket createRestApi() {// 创建API基本信息
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.framework"))// 扫描该包下的所有需要在Swagger中展示的API，@ApiIgnore注解标注的除外
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {// 创建API的基本信息，这些信息会在Swagger UI中进行显示
+        return new ApiInfoBuilder()
+                .title("dynamic测试项目")// API 标题
+                .description("spring-cloud")// API描述
+                .contact(new Contact("zhouwenlai", "", "zhouwenlai@chehejia.com"))// 联系人
+                .version("1.0.0.0")// 版本号
+                .build();
+    }
+
+    @ApiOperation("hi interface")
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
