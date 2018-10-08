1c6e996ac2753861a64e6736fe11b2718aaaa895
zhouwenlai <zhouwenlai@chehejia.com>
2018-10-08 10:24:41 +0800

    add deps

--- boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
+++ boot/src/main/java/com/framework/dynamic/boot/Bootstrap.java
@@ -1,28 +1,17 @@
 package com.framework.dynamic.boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootConfiguration
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class Bootstrap {
 
-    public static void main(final String[] args) throws Exception {
-        //Undertow server = Undertow.builder()
-        //        .addHttpListener(8080, "localhost")
-        //        .setHandler(exchange -> {
-        //            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-        //            exchange.getResponseSender().send("Hello World");
-        //        }).build();
-        //server.start();
-        //GroovyScriptEngine engine = new GroovyScriptEngine();
-        //engine.getGroovyClassLoader().parseClass(
-        //engine.run("", "");
-        //new SpringApplicationBuilder(Bootstrap.class).web(WebApplicationType.SERVLET).build().run(args);
-
-        Logger log = LoggerFactory.getLogger("root");
-        for (int i = 0; i < 200; i++) {
-            log.error("hello world");
-        }
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(Bootstrap.class).web(WebApplicationType.SERVLET).build().run(args);
     }
 }
